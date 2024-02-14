e8ab2c9c9e52b79868f778daa11e4e88825dda8a
Shane Deocales <126802691+ShaneyDee@users.noreply.github.com>
2024-02-14 18:31:13 +0000

    Final

--- DateTask.java
+++ DateTask.java
@@ -1,1 +1,63 @@
+public class DateTask {
+    private int day;
+    private int month;
+    private int year;
+
+    // Constructor
+    public DateTask() {
+        this(1, 1, 1);
+    }
+
+    public DateTask(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    // Getter and Setter methods
+    public int getDay() {
+        return day;
+    }
+
+    public int setDay(int day) {
+        if (day > 0 && day <= 31) {
+            this.day = day;
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int setMonth(int month) {
+        if (month > 0 && month <= 12) {
+            this.month = month;
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int setYear(int year) {
+        if (year > 0) {
+            this.year = year;
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    // toString method to display "12/12/2012" date format
+    @Override
+    public String toString() {
+        return String.format("%02d/%02d/%04d", month, day, year);
+    }
+}
 
--- MyDate.java
+++ MyDate.java
@@ -1,1 +1,22 @@
+public class MyDate {
+    public static void main(String[] args) {
+        DateTask date1 = new DateTask(1, 1, 1978);
+        DateTask date2 = new DateTask(21, 9, 1984);
+
+        System.out.println("Date 1: " + date1);
+        System.out.println("Date 2: " + date2);
+
+        System.out.println("Leap years between " + date1 + " and " + date2 + ":");
+        for (int year = date1.getYear(); year <= date2.getYear(); year++) {
+            if (isLeapYear(year)) {
+                System.out.println(year);
+            }
+        }
+    }
+
+    // Method to check for leap year
+    public static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+}
 
